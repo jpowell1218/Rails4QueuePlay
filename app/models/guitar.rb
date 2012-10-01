a68b79ef868e01cbbff06e94b27a23e2a229079f
Jeff Powell <powellj@mac.com>
2012-09-30 20:27:49 -0600

    Added GuitarReviewsSearch class + some basic code in the guitar model

--- app/models/guitar.rb
+++ app/models/guitar.rb
@@ -1,2 +1,17 @@
 class Guitar < ActiveRecord::Base
+  attr_accessible :model, :year
+
+  def register
+    self.save && search_guitar_reviews
+  end
+
+  def to_s
+    self-model + self.year.to_s
+  end
+
+  private
+    
+    def search_guitar_reviews
+
+    end
 end
---
+++ app/models/guitar_reviews_search.rb
@@ -0,0 +1,17 @@
+class GuitarReviewsSearch
+  
+  def initialize(name)
+    @name = name
+  end
+
+  def run
+    p ""
+    p ""
+    p "search reviews for #{@name}"
+    sleep 10
+    p "done!"
+    p ""
+    p ""
+  end
+
+end
