0543944c91f3af3c77037608e1129d12947e6db1
Jeff Powell <powellj@mac.com>
2012-09-30 20:15:29 -0600

    Initial Rails 4 based app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
+GuitarsUnlimited::Application.config.secret_token = '5f44f28ae330f6e0b9dc567c6426a69dcb25d60ffbf360febf6370371a251074dbf9e127e11193695449de8552371ae5cdefd4660f4b803483cbbd6e06e78727'
